75d67752166f14b8c27f1df29831efdb7c01ee04
Paul Stonebraker <wpstonebraker@gmail.com>
2020-11-24 17:28:05 -0500

    end of day

--- skeleton/lib/05_aggregates.rb
+++ skeleton/lib/05_aggregates.rb
@@ -95,5 +95,13 @@ end
 def populous_continents
   # List the continents that have a total population of at least 100 million.
   execute(<<-SQL)
+  SELECT
+    continent
+  FROM
+    countries
+  WHERE
+    population >= 100000000
+  GROUP BY
+    continent
   SQL
 end
